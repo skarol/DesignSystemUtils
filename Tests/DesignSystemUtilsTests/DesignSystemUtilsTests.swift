8366ee959e1e01920616cfa4cb8a2516d19027cd
Karolina Samorek <karolina.samorek@gmail.com>
2022-05-12 12:30:59 +0200

    Remove DesignSystemUtils file

--- Tests/DesignSystemUtilsTests/DesignSystemUtilsTests.swift
+++ Tests/DesignSystemUtilsTests/DesignSystemUtilsTests.swift
@@ -6,6 +6,6 @@ final class DesignSystemUtilsTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(DesignSystemUtils().text, "Hello, World!")
+        XCTAssertTrue(true)
     }
 }
